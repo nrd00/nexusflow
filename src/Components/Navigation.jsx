a88e239ee7d627aab83ea9aad458cf3c32c79b3c
Naimur Rahman Dorjoy <rahmandurjoy9@gmail.com>
2026-09-09 17:38:17 +0600

    Folder & File initialization

---
+++ src/Components/Navigation.jsx
@@ -0,0 +1,19 @@
+import { CiMenuFries } from "react-icons/ci";
+
+
+
+
+
+const Navigation = () => {
+
+    return(
+        <>
+        <nav>
+            <a href="#">NexusFlow</a>
+            <CiMenuFries />
+        </nav>
+        </>
+    )
+}
+
+export default Navigation;
